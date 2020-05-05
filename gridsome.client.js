0ae272333296b3022afe879e9068f2efbc8ea70a
marcvangend <info@marcvangend.nl>
2020-05-05 20:47:59 +0200

    replace obsolete debug option

--- gridsome.client.js
+++ gridsome.client.js
@@ -2,9 +2,7 @@ import VueGtag from "vue-gtag";
 
 export default function (Vue, options, { isServer: disabled, router }) {
   Vue.use(VueGtag, {
-    debug: {
-      sendHitTask: process.env.NODE_ENV === 'production'
-    },
+    disableScriptLoad: process.env.NODE_ENV === 'production',
     router,
     disabled,
     ...options
